8a4a61e394d2b970708b798bcdb6bd04e60860b7
sayam3004 <mutant.sayam>
2025-11-13 21:16:13 +0600

    created serverjs

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
