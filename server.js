0f51f1de22c564efcee7a63b3d6e52460decc54a
sayam3004 <mutant.sayam>
2025-11-13 21:17:37 +0600

    setup DB

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'task_manager',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        process.exit(1);
+    }
+    console.log('Connected to MySQL database');
+});
