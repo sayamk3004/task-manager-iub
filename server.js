3b2e045777a8a70f27b68ad39979d8f1d0494490
sayam3004 <mutant.sayam>
2025-11-13 21:18:48 +0600

    upload multer setup

--- server.js
+++ server.js
@@ -24,3 +24,13 @@ db.connect((err) => {
     }
     console.log('Connected to MySQL database');
 });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage });
